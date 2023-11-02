d4d7bbca9874d4415cab1826307e166cd1526b97
SherlockZhan-g <143365367+SherlockZhan-g@users.noreply.github.com>
2023-11-02 16:35:05 -0500

    Update Part_IV.m

--- Part_IV.m
+++ Part_IV.m
@@ -13,15 +13,15 @@ dtrend = movmean(cumulativeDeaths,w);
 plot(dtrend.')
 
 figure;
-plot([cumulativeDeaths;newInfections].');
+plot([cumulativeDeaths;cumsum(newInfections)].');
 % *******************************************************************
 x0 = [1-newInfections(1)-cumulativeDeaths(1),newInfections(1),0,0,cumulativeDeaths(1),0];
 
 v_rate = 0;
-inf_rate1 = 0.005;
+inf_rate1 = 0.0054;
 inf_rate2 = 0;
 immune_rate = 0.3;
-death1 = 0.06;
+death1 = 0.055;
 death2 = 0;
 recover1 = 0.2;
 recover2 = 0;
@@ -35,14 +35,14 @@ A = [1-inf_rate1-v_rate recover1                      0                 rein_rat
      v_rate             0                             recover2          0           0 1-inf_rate2 ];
 
 B = zeros(6,1);
-t = 1:100;
+t = 1:120;
 
 
 y = modelVaccine(A,B,t,x0);
 
 
-model_newinf = [x0(2)+x0(3),(y(:,1)*inf_rate1 + y(:,6)*inf_rate2).'];
-model_newinf = model_newinf(1:100);
+model_newinf = cumsum([x0(2)+x0(3),(y(:,1)*inf_rate1 + y(:,6)*inf_rate2).']);
+model_newinf = model_newinf(1:120);
 
  
 figure
@@ -51,18 +51,18 @@ tiledlayout(1,2)
 nexttile
 plot(model_newinf);
 hold on;
-plot(newInfections(1:100).');
+plot(cuminfections(1:120).');
 hold off;
 nexttile
 plot(y(:,5));
 hold on;
-plot(cumulativeDeaths(1:100).');
+plot(cumulativeDeaths(1:120).');
 % *******************************************************************
 v_rate = 0;
-inf_rate1 = 0.01;
+inf_rate1 = 0.025;
 inf_rate2 = 0;
 immune_rate = 0.3;
-death1 = 0.06;
+death1 = 0.013;
 death2 = 0;
 recover1 = 0.2;
 recover2 = 0;
@@ -76,13 +76,13 @@ A = [1-inf_rate1-v_rate recover1                      0                 rein_rat
      v_rate             0                             recover2          0           0 1-inf_rate2 ];
 
 B = zeros(6,1);
-t = (101:135)-100;
-x0 = A*y(100,:).';
+t = (121:145)-120;
+x0 = A*y(120,:).';
 
 y1 = modelVaccine(A,B,t,x0);
 
 
-model_newinf2 = model_newinf(100)+(y1(:,1)*inf_rate1 + y1(:,6)*inf_rate2).';
+model_newinf2 = model_newinf(120)+cumsum(y1(:,1)*inf_rate1 + y1(:,6)*inf_rate2).';
 
  
 figure
@@ -91,22 +91,22 @@ tiledlayout(1,2)
 nexttile
 plot(model_newinf2);
 hold on;
-plot((newInfections(101:135).'));
+plot(cuminfections(121:145).');
 hold off;
 nexttile
 plot(y1(:,5));
 hold on;
-plot(cumulativeDeaths(101:135).');
+plot(cumulativeDeaths(121:145).');
 % *******************************************************************
-v_rate = 0.3;
-inf_rate1 = 0.0055;
-inf_rate2 = 0.001;
+v_rate = 0.235;
+inf_rate1 = 0.025;
+inf_rate2 = 0.005;
 immune_rate = 0.5;
-death1 = 0.085;
-death2 = 0.001;
+death1 = 0.013;
+death2 = 0.00001;
 recover1 = 0.215;
-recover2 = 0.615;
-rein_rate = 0.3;
+recover2 = 0.315;
+rein_rate = 0.4;
 
 A = [1-inf_rate1-v_rate recover1                      0                 rein_rate   0 0
      inf_rate1          1-recover1-death1-immune_rate 0                 0           0 0
@@ -116,15 +116,15 @@ A = [1-inf_rate1-v_rate recover1                      0                 rein_rat
      v_rate             0                             recover2          0           0 1-inf_rate2 ];
 
 
-x0 = A*y1(35,:).';
+x0 = A*y1(25,:).';
 B = zeros(6,1);
-t = (136:400)-135;
+t = (145:400)-144;
 
 
 y2 = modelVaccine(A,B,t,x0);
 
 
-model_newinf3 = model_newinf2(15)+(y2(:,1)*inf_rate1 + y2(:,6)*inf_rate2).';
+model_newinf3 = model_newinf2(25)+cumsum(y2(:,1)*inf_rate1 + y2(:,6)*inf_rate2).';
 
  
 figure
@@ -133,12 +133,12 @@ tiledlayout(1,2)
 nexttile
 plot(model_newinf3);
 hold on;
-plot(newInfections(136:400).');
+plot(cuminfections(145:400).');
 hold off;
 nexttile
 plot(y2(:,5));
 hold on;
-plot(cumulativeDeaths(136:400).');
+plot(cumulativeDeaths(145:400).');
 % *******************************************************************
 %% Define functions
 function Y = model(A,B,t,x0)
