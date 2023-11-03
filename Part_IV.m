c76a65b388f945b9a5e2a3e3e171db692d810fdc
SherlockZhan-g <143365367+SherlockZhan-g@users.noreply.github.com>
2023-11-03 03:03:54 -0500

    Update Part_IV.m

--- Part_IV.m
+++ Part_IV.m
@@ -1,152 +1,87 @@
 clc;
 close all;
 clearvars;
-
-load("mockdata2023.mat");
-
-
-
-w = 30;
-cuminfections = cumsum(newInfections);
-itrend = movmean(cuminfections,w);
-dtrend = movmean(cumulativeDeaths,w);
-plot(dtrend.')
-
-figure;
-plot([cumulativeDeaths;cumsum(newInfections)].');
 % *******************************************************************
+%% load the data
+load("mockdata2023.mat");
+plot([cumulativeDeaths;newInfections].');
+% *******************************************************************
+%% Before roll out
+% initialize the initial state
 x0 = [1-newInfections(1)-cumulativeDeaths(1),newInfections(1),0,0,cumulativeDeaths(1),0];
-
+% parameters for the matrix
 v_rate = 0;
-inf_rate1 = 0.0054;
-inf_rate2 = 0;
-immune_rate = 0.3;
-death1 = 0.055;
-death2 = 0;
-recover1 = 0.2;
-recover2 = 0;
-rein_rate = 0.05;
-
-A = [1-inf_rate1-v_rate recover1                      0                 rein_rate   0 0
-     inf_rate1          1-recover1-death1-immune_rate 0                 0           0 0
-     0                  0                             1-recover2-death2 0           0 inf_rate2
-     0                  immune_rate                   0                 1-rein_rate 0 0 
-     0                  death1                        death2            0           1 0
-     v_rate             0                             recover2          0           0 1-inf_rate2 ];
-
+inf_rate1 = 0.005;
+inf_rate2 = 0.2;
+immune_rate = 0.05;
+immune_rate2 = 0.1;
+death1 = 0.02;
+death2 = 0.00005;
+recover1 = 0.1;
+recover2 = 0.07;
+% update matrix
+A = [1-inf_rate1-v_rate recover1                      0                              0            0 0
+     inf_rate1          1-recover1-death1-immune_rate 0                              0            0 0
+     0                  0                             1-recover2-death2-immune_rate2 0            0 inf_rate2
+     0                  immune_rate                   immune_rate2                   1            0 0
+     0                  death1                        death2                         0            1 0
+     v_rate             0                             recover2                       0            0 1-inf_rate2 ];
+% initialize time span
 B = zeros(6,1);
 t = 1:120;
-
-
+% use the function to model
 y = modelVaccine(A,B,t,x0);
-
-
-model_newinf = cumsum([x0(2)+x0(3),(y(:,1)*inf_rate1 + y(:,6)*inf_rate2).']);
+% calculate the new cases from the model
+model_newinf = ([x0(2)+x0(3),(y(:,1)*inf_rate1 + y(:,6)*inf_rate2).']);
 model_newinf = model_newinf(1:120);
-
- 
-figure
-tiledlayout(1,2)
-
-nexttile
-plot(model_newinf);
-hold on;
-plot(cuminfections(1:120).');
-hold off;
-nexttile
-plot(y(:,5));
-hold on;
-plot(cumulativeDeaths(1:120).');
 % *******************************************************************
-v_rate = 0;
-inf_rate1 = 0.025;
-inf_rate2 = 0;
-immune_rate = 0.3;
-death1 = 0.013;
-death2 = 0;
-recover1 = 0.2;
-recover2 = 0;
-rein_rate = 0.05;
+%% After roll out
+% change the values for the matrix
+v_rate = 0.045;
+inf_rate1 = 0.005;
+inf_rate2 = 0.15;
+immune_rate = 0.05;
+immune_rate2 = 0.1;
+death1 = 0.02;
+death2 = 0.00005;
+recover1 = 0.1;
+recover2 = 0.07;
 
-A = [1-inf_rate1-v_rate recover1                      0                 rein_rate   0 0
-     inf_rate1          1-recover1-death1-immune_rate 0                 0           0 0
-     0                  0                             1-recover2-death2 0           0 inf_rate2
-     0                  immune_rate                   0                 1-rein_rate 0 0 
-     0                  death1                        death2            0           1 0
-     v_rate             0                             recover2          0           0 1-inf_rate2 ];
-
-B = zeros(6,1);
-t = (121:145)-120;
+A = [1-inf_rate1-v_rate recover1                      0                              0           0 0
+     inf_rate1          1-recover1-death1-immune_rate 0                              0           0 0
+     0                  0                             1-recover2-death2-immune_rate2 0           0 inf_rate2
+     0                  immune_rate                   immune_rate2                   1           0 0
+     0                  death1                        death2                         0           1 0
+     v_rate             0                             recover2                       0           0 1-inf_rate2 ];
+% update the inital state and the time span
 x0 = A*y(120,:).';
-
+B = zeros(6,1);
+t = (121:400)-120;
+% call the function and model.
 y1 = modelVaccine(A,B,t,x0);
 
-
-model_newinf2 = model_newinf(120)+cumsum(y1(:,1)*inf_rate1 + y1(:,6)*inf_rate2).';
-
- 
-figure
-tiledlayout(1,2)
-
-nexttile
-plot(model_newinf2);
-hold on;
-plot(cuminfections(121:145).');
-hold off;
-nexttile
-plot(y1(:,5));
-hold on;
-plot(cumulativeDeaths(121:145).');
+model_newinf2 = (y1(:,1)*inf_rate1 + y1(:,6)*inf_rate2).';
 % *******************************************************************
-v_rate = 0.235;
-inf_rate1 = 0.025;
-inf_rate2 = 0.005;
-immune_rate = 0.5;
-death1 = 0.013;
-death2 = 0.00001;
-recover1 = 0.215;
-recover2 = 0.315;
-rein_rate = 0.4;
-
-A = [1-inf_rate1-v_rate recover1                      0                 rein_rate   0 0
-     inf_rate1          1-recover1-death1-immune_rate 0                 0           0 0
-     0                  0                             1-recover2-death2 0           0 inf_rate2
-     0                  immune_rate                   0                 1-rein_rate 0 0 
-     0                  death1                        death2            0           1 0
-     v_rate             0                             recover2          0           0 1-inf_rate2 ];
-
-
-x0 = A*y1(25,:).';
-B = zeros(6,1);
-t = (145:400)-144;
-
-
-y2 = modelVaccine(A,B,t,x0);
-
-
-model_newinf3 = model_newinf2(25)+cumsum(y2(:,1)*inf_rate1 + y2(:,6)*inf_rate2).';
-
- 
-figure
-tiledlayout(1,2)
-
-nexttile
-plot(model_newinf3);
+%% Combine the model and give output
+% combine the model and the model-based newinfection.
+x = [y;y1];
+model_newinfection = [model_newinf,model_newinf2];
+% use immse function to callibrate the parameters.
+err1 = immse(x(:,5).',cumulativeDeaths);
+err2 = immse(model_newinfection,newInfections);
+% plot the figure 
+figure;
+plot([x(:,5).';model_newinfection].');
 hold on;
-plot(cuminfections(145:400).');
-hold off;
-nexttile
-plot(y2(:,5));
-hold on;
-plot(cumulativeDeaths(145:400).');
+plot([cumulativeDeaths;newInfections].')
+title('Model with Vaccine')
+legend('cumulative deaths (model)','new infection (model)','cumulative deaths','new infection','Location','northwest');
+% calculate the vaxpop and vaxbreak and export
+vaxpop = cumsum([y(:,1)*0;y1(:,1)*v_rate]);
+vaxbreak = x(:,3);
+save("competiton.mat",'vaxpop','vaxbreak');
 % *******************************************************************
 %% Define functions
-function Y = model(A,B,t,x0)
-size1 = size(t,2);
-sys_sir_base = ss(A,B,eye(4),zeros(4,1),1);
-Y = lsim(sys_sir_base,zeros(size1,1),t,x0);
-end
-
 function Y = modelVaccine(A,B,t,x0)
 size1 = size(t,2);
 sys_sir_base = ss(A,B,eye(6),zeros(6,1),1);
